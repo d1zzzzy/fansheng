5878e5766c1345df77298f5468ed4c6c6bdc02db
daizhitao <daizhitao@dlz-tech.com>
2024-12-18 14:33:44 +0800

    fix: 调整 meta 数据的写入方式

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,7 @@
-'use client';
-
-import Head from "next/head";
-import { usePathname } from "next/navigation";
 import { Geist, Geist_Mono } from "next/font/google";
 
 import "./globals.css";
+import { Metadata } from "next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -16,45 +13,31 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const isProd = process.env.NODE_ENV === 'production';
+const basePath = isProd ? `${process.env.NEXT_PUBLIC_ASSET_PREFIX}` : '';
+
+export const metadata: Metadata = {
+  title: '凡生',
+  description: '凡生的个人网站——前端工程师、极限运动员、闲来拍几张',
+  keywords: ['凡生', '前端工程师', '极限运动员', '摄影'],
+  icons: {
+    icon: `${basePath}/favicon.ico`,
+    shortcut: `${basePath}/favicon.ico`,
+    apple: `${basePath}/apple-touch-icon.png`,
+    other: {
+      rel: 'icon',
+      url: `${basePath}/favicon.ico`,
+    },
+  },
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const pathname = usePathname();
-  console.log(pathname);
-  const basePath = pathname.startsWith('/') ? pathname : `/${pathname}`;
-
   return (
     <html lang="en">
-      <Head>
-        {/* 默认 favicon */}
-        <link rel="icon" href={`${basePath}/favicon.ico`} />
-
-        {/* 16x16 favicon */}
-        <link rel="icon" type="image/png" sizes="16x16" href={`${basePath}/favicon-16x16.png`} />
-
-        {/* 32x32 favicon */}
-        <link rel="icon" type="image/png" sizes="32x32" href={`${basePath}/favicon-32x32.png`} />
-
-        {/* Apple Touch Icon */}
-        <link rel="apple-touch-icon" sizes="180x180" href={`${basePath}/apple-touch-icon.png`} />
-
-        {/* Manifest 文件（如果有） */}
-        <link rel="manifest" href={`${basePath}/manifest.json`} />
-
-        {/* 浏览器主题颜色 */}
-        <meta name="theme-color" content="#ffffff" />
-
-        {/* 网站描述 */}
-        <meta name="description" content="凡生的个人网站——前端工程师、极限运动员、闲来拍几张" />
-
-        {/* 网站关键词 */}
-        <meta name="keywords" content="凡生, 前端工程师, 极限运动员, 摄影" />
-
-        {/* 网站标题 */}
-        <title>凡生</title>
-      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
