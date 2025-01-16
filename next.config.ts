5dbb54daaa468962777158ab091f4dcfd65b7a7f
daizhitao <d1zzzy.fe@gmail.com>
2025-01-16 23:17:51 +0800

    feat: 删除 JavaScript 循环相关文档并简化 next.config.ts 配置

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
 import { resolve } from "path";
 
-const isProd = process.env.NODE_ENV === 'production';
-const repoName = 'fansheng';
-
 module.exports = {
-  output: 'export',
-  basePath: isProd ? `/${repoName}` : '',
-  assetPrefix: isProd ? `/${repoName}/` : '',
   images: {
     unoptimized: true,
   },
